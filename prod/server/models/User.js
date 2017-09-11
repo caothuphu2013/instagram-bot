aeb9bad45646eefdcd6dbe87d7dd6f46d40c57e9
Colin Johnson <cjohnson@theprogrampdx.com>
2017-09-11 15:32:26 -0700

    restructure root folders

--- prod/server/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
-
-const userSchema = new Schema({
-  instaID: String
-})
-
-mongoose.model('users', userSchema)
--- src/Auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = {
-  domain: 'colinj.auth0.com',
-  clientId: 'R51SJ5BRNlsIW0LrbjtvlZ_XF1M2uSnt',
-  callbackUrl: 'http://localhost:3000/callback'
-}
